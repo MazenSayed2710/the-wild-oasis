87f491a1912c7d56d719cb7e0207065b259e1627
Mazen <ma3en.mohamed@gmail.com>
2025-01-13 20:10:54 +0200

    add env file

--- src/servicse/supabase.js
+++ src/servicse/supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://xurghvxrcsbvuhbkjnlk.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cmdodnhyY3NidnVoYmtqbmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkxMzEzMjQsImV4cCI6MjAxNDcwNzMyNH0.BvbVjQEx0sf2CjKjGY5cF3eH--3aw4VJ6bDuGDNKnc8";
+export const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
